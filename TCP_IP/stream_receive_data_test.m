981df625539545e11b46ab52e2fcca561abe1df7
fvalerio <valerio.francioni@hotmail.it>
2025-04-02 14:01:45 -0400

    commit

---
+++ TCP_IP/stream_receive_data_test.m
@@ -0,0 +1,59 @@
+clear all %#ok<CLALL>  % Clear all variables from the workspace
+close all             % Close all open figure windows
+
+%% INSERT YOUR RECEIVER IPv4 ADDRESS HERE
+IPv4 = '127.0.0.1';
+
+%% RUN THE REST
+% Define Image Dimensions and Preallocate Storage
+xpixels = 512;         % Image height (number of rows)
+ypixels = 796;         % Image width (number of columns)
+frames  = 10000;         % Total number of frames expected to receive
+% Preallocate a 3D array (uint16) to store the incoming image frames.
+received_Data = uint16(zeros(xpixels, ypixels, frames));
+
+% Set Up TCP/IP Connection Parameters
+port = 30001;          % Port number used for communication
+buffsz = 20;           % Buffer size multiplier to ensure the input buffer is large enough
+
+% Create a TCP/IP client object to receive data from the local host.
+% '127.0.0.1' is the loopback IP address (localhost), which is used for same-machine communication.
+% 'InputBufferSize' is set to accommodate the expected number of pixels per frame times the buffer multiplier.
+% 'Terminator' specifies the end-of-line characters used in the communication protocol.
+tcpipServer = tcpip(IPv4, port, 'NetworkRole', 'client', ...
+    'InputBufferSize', (xpixels*ypixels*buffsz), 'Terminator', 'CR/LF');
+
+% Open the TCP/IP connection.
+fopen(tcpipServer);
+
+% Initialize Figure for Displaying Incoming Images
+% Create a figure window for displaying the received images.
+hFig = figure(1);
+% Create an axes object in the figure.
+hAx  = axes('Parent', hFig);
+% Initialize the display with a blank image.
+% The image object handle (hImg) will be used to update the displayed image data.
+hImg = imshow(zeros(xpixels, ypixels, 'uint16'), 'Parent', hAx);
+
+tt = nan(1, frames);
+tic
+% Receive and Display Frames in a Loop
+iframe = 0;
+while iframe <= frames - 1
+    % Read image data from the TCP/IP connection.
+    % The number of elements to read is equal to the total number of pixels in the image.
+    if tcpipServer.BytesAvailable >= xpixels * ypixels * 2
+        vect_img = uint16(fread(tcpipServer, xpixels*ypixels, 'uint16'));
+        iframe = iframe + 1;
+        tt(iframe) = toc;
+    else
+        continue
+    end
+
+end
+
+% Clean Up the TCP/IP Connection
+% Close the TCP/IP connection once all frames have been received.
+fclose(tcpipServer);
+% Clear the TCP/IP object from the workspace.
+clear tcpipServer
--- TCP_IP/stream_send_data_test.m
+++ TCP_IP/stream_send_data_test.m
@@ -1,10 +1,13 @@
 clear all %#ok<CLALL>  % Clear all workspace variables
-
-% Load the image stack from the specified .mat file.
+%% INSERT YOUR CLOOSE DIRECTORY HERE
 pth2CLOOSE = 'C:\Users\MysticSocialist\Documents\GitHub\CLOOSE_MS';
+%% AND YOUR RECEIVER IPv4 ADDRESS HERE
+IPv4 = '127.0.0.1';
 
-pth = []
-load('C:\Users\MysticSocialist\Documents\GitHub\CLOOSE_MS\Generate_images\WelcomeMsg.mat')
+%% RUN THE REST
+% Load the image stack from the specified .mat file.
+pth = [pth2CLOOSE '\Generate_images\WelcomeMsg.mat'];
+load(pth)
 
 % Define TCP/IP communication parameters.
 port = 30001;  % Set the port number for communication.
@@ -15,18 +18,18 @@ buffsz = 20;   % Buffer size multiplier for the output buffer.
 % 'NetworkRole' is set to 'server' for this instance.
 % 'OutputBufferSize' is determined by the image dimensions multiplied by the buffer size factor.
 % 'Terminator' is set to 'CR/LF' (Carriage Return/Line Feed).
-send_device = tcpip('10.29.233.45', port, 'NetworkRole', 'server', ...
+send_device = tcpip(IPv4, port, 'NetworkRole', 'server', ...
     'OutputBufferSize', (size(stack, 1) * size(stack, 2) * buffsz), 'Terminator', 'CR/LF');
+
+% Reshape the 2D image frame into a column vector.
+% This prepares the data for transmission over the TCP/IP connection.
+vect_img = reshape(stack(:, :, 1), [], 1);
 
 % Open the TCP/IP connection.
 fopen(send_device);
 
 % Loop through each frame in the image stack.
-for iframe = 1:size(stack, 3)
-    % Reshape the 2D image frame into a column vector.
-    % This prepares the data for transmission over the TCP/IP connection.
-    vect_img = reshape(stack(:, :, iframe), [], 1);
-    
+for iframe = 1:10000
     % Write the image data to the TCP/IP connection as uint16.
     fwrite(send_device, vect_img, 'uint16');
 end
